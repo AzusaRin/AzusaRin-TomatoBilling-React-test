25c38249f0016ceb902d692e2f96929c67b17c52
Azusa <qq745847188@gmail.com>
2022-08-08 15:53:01 +0800

    抽离FormItem组件

---
+++ src/components/FormItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-left: 16px;
+  background-color: white;
+
+  .noteName {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 3.5rem;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+    font-size: 14px;
+  }
+`;
+type Props = {
+  label: string
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input: React.FunctionComponent<Props> = (props) => {
+  const {label, children, ...rest} = props;
+  return (
+    <Label>
+      <span className="noteName">{props.label}</span>
+      <input {...rest}
+      />
+    </Label>
+  );
+};
+
+export {Input};
