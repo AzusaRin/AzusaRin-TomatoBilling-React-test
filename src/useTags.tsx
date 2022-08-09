c65be29a13f5194c6931b2036bad0566eeaa6a19
Azusa <qq745847188@gmail.com>
2022-08-09 10:53:36 +0800

    优化变更标签和删除标签的代码

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -6,30 +6,14 @@ const defaultTags = [
 ];
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
-  const findTagIndex = (id: number) => {
-    let result = -1;
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].id === id) {
-        result = i;
-        break;
-      }
-    }
-    return result;
-  };
   const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+    setTags(tags.map(tag => tag.id === id ? {id, name:obj.name} : tag));
   };
 
-  const deleteTag = (id:number) =>{
-    const index = findTagIndex(id)
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index,1)
-    setTags(tagsClone)
-  }
-  return {tags, setTags, updateTag, findTagIndex,deleteTag};
+  const deleteTag = (id: number) => {
+    setTags(tags.filter(tag => tag.id !== id));
+  };
+  return {tags, setTags, updateTag, deleteTag};
 };
 
 export {useTags};
