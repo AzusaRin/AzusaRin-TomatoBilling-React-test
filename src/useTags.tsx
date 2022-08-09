700a808a91cd05e2210d78908906bb4b847068e3
Azusa <qq745847188@gmail.com>
2022-08-09 09:04:41 +0800

    编辑标签删除功能

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -17,13 +17,19 @@ const useTags = () => {
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
-    const index = (id: number) => findTagIndex(id);
-    //深拷贝tags变成tagsClone
+    const index = findTagIndex(id);
     const tagsClone = JSON.parse(JSON.stringify(tags));
     tagsClone.splice(index, 1, {id: id, name: obj.name});
     setTags(tagsClone);
   };
-  return {tags, setTags, updateTag, findTagIndex};
+
+  const deleteTag = (id:number) =>{
+    const index = findTagIndex(id)
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index,1)
+    setTags(tagsClone)
+  }
+  return {tags, setTags, updateTag, findTagIndex,deleteTag};
 };
 
 export {useTags};
--- src/views/EditLabel.tsx
+++ src/views/EditLabel.tsx
@@ -15,20 +15,28 @@ const Header = styled.div`
   background: rgb(253, 167, 168);
   line-height: 20px;
   padding: 14px;
+  font-size: 18px;
 `;
 const InputWrapper = styled.div`
   background: white;
   padding: 0 16px;
   margin-top: 16px;
 `;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 type Params = {
   id: string
 }
 const EditLabel: React.FunctionComponent = () => {
-  const {tags, updateTag} = useTags();
-  let {id:idString} = useParams<Params>();
+  const {tags, updateTag, deleteTag} = useTags();
+  let {id: idString} = useParams<Params>();
   const tag = tags.filter(t => t.id === parseInt(idString!))[0];
+
+
   return (
     <LabelWrapper>
       <Main>
@@ -37,18 +45,25 @@ const EditLabel: React.FunctionComponent = () => {
           <span>编辑标签</span>
           <Icon/>
         </Header>
-        <InputWrapper>
-          <Input label="标签名" type="text" placeholder="标签名" value={tag.name}
-                 onChange={(event) => updateTag(tag.id, {name: event.target.value})}/>
-        </InputWrapper>
-        <Button>
-          <Icon name="deleteTag"/>
-          删除标签
-        </Button>
+        {tag ? <div>
+          <InputWrapper>
+            <Input label="标签名" type="text" placeholder="标签名" value={tag.name}
+                   onChange={(event) => updateTag(tag.id, {name: event.target.value})}/>
+          </InputWrapper>
+          <ButtonWrapper>
+            <Button onClick={() => deleteTag(tag.id)}>
+              <Icon name="deleteTag"/>
+             <span>删除标签</span>
+            </Button>
+          </ButtonWrapper>
+        </div> : <div>标签不存在</div>}
       </Main>
       <Nav/>
     </LabelWrapper>
+
+
   );
+
 };
 
 export {EditLabel};
